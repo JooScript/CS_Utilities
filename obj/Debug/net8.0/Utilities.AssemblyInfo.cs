9774600360499a994c4f1771305f7ec6de686d25
Yousef <businessyousef00@gmail.com>
2025-07-14 10:35:46 +0300

    Update GitIgnore

--- obj/Debug/net8.0/Utilities.AssemblyInfo.cs
+++ obj/Debug/net8.0/Utilities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b24c056aae342d1a8e6677e0e261c80213a1df3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e32f74c0abaaea7e1e1b399d65b05bcc0e10f219")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
