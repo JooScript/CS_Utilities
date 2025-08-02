c3e1a87de96d9db92b563aa2361ed94661128af9
Yousef <businessyousef00@gmail.com>
2025-08-02 14:34:19 +0300

    Uodate Structure

--- obj/Debug/net8.0/Utilities.AssemblyInfo.cs
+++ obj/Debug/net8.0/Utilities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c87ed486dc00c62c0dca2ce45569c2565a6833")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+350983be747900e65bc1d0fe506dc7a74e8fb834")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
