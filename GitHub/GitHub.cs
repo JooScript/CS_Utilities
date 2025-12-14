e87dcd8ef93fd0996d034c9460abc509b4e0fdd2
Yousef <160142268+JooScript@users.noreply.github.com>
2025-12-14 22:13:30 +0200

    Add GitHub Class

---
+++ GitHub/GitHub.cs
@@ -0,0 +1,147 @@
+﻿using LibGit2Sharp;
+using Octokit;
+using GitCredentials = LibGit2Sharp.UsernamePasswordCredentials;
+using GitHubCredentials = Octokit.Credentials;
+using GitHubRepository = Octokit.Repository;
+using GitRepository = LibGit2Sharp.Repository;
+using GitSignature = LibGit2Sharp.Signature;
+
+public class GitHub
+{
+    private string _Token { get; }
+    private GitHubClient _Client { get; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    private GitSignature _Signature => new GitSignature(Name, Email, DateTimeOffset.Now);
+
+    public GitHub(string token, string name, string email)
+    {
+        _Token = token;
+        _Client = new GitHubClient(new ProductHeaderValue("GitHubSyncService"))
+        {
+            Credentials = new GitHubCredentials(_Token)
+        };
+        Name = name;
+        Email = email;
+    }
+
+    #region GIT (LibGit2Sharp)
+
+    public void CloneIfMissing(string repoUrl, string localPath)
+    {
+        if (!Directory.Exists(localPath))
+        {
+            var co = new CloneOptions();
+
+            co.FetchOptions.CredentialsProvider = (_url, _user, _cred) =>
+                new UsernamePasswordCredentials
+                {
+                    Username = "token",
+                    Password = _Token
+                };
+
+            GitRepository.Clone(repoUrl, localPath, co);
+        }
+    }
+
+    public GitRepository OpenRepository(string localPath)
+    {
+        return new GitRepository(localPath);
+    }
+
+    public void Pull(GitRepository repo)
+    {
+        Commands.Pull(repo, _Signature, new PullOptions
+        {
+            FetchOptions = new FetchOptions
+            {
+                CredentialsProvider = (_, _, _) => new GitCredentials
+                {
+                    Username = "token",
+                    Password = _Token
+                }
+            }
+        });
+    }
+
+    public bool HasChanges(GitRepository repo)
+    {
+        return repo.RetrieveStatus().IsDirty;
+    }
+
+    public void StageAll(GitRepository repo)
+    {
+        Commands.Stage(repo, "*");
+    }
+
+    public void Commit(GitRepository repo, string message)
+    {
+        repo.Commit(message, _Signature, _Signature);
+    }
+
+    public void Push(GitRepository repo)
+    {
+        repo.Network.Push(repo.Head, new PushOptions
+        {
+            CredentialsProvider = (_, _, _) => new GitCredentials
+            {
+                Username = "token",
+                Password = _Token
+            }
+        });
+    }
+
+    #endregion
+
+    #region GITHUB (Octokit)
+
+    public async Task<User> GetCurrentUserAsync()
+    {
+        return await _Client.User.Current();
+    }
+
+    public async Task<GitHubRepository> CreateRepositoryAsync(string name, bool isPrivate)
+    {
+        return await _Client.Repository.Create(new NewRepository(name) { Private = isPrivate });
+    }
+
+    public async Task<IReadOnlyList<GitHubRepository>> GetRepositoriesAsync()
+    {
+        return await _Client.Repository.GetAllForCurrent();
+    }
+
+    public async Task<Issue> CreateIssueAsync(string owner, string repo, string title, string body)
+    {
+        return await _Client.Issue.Create(owner, repo, new NewIssue(title) { Body = body });
+    }
+
+    public async Task<PullRequest> CreatePullRequestAsync(string owner, string repo, string title, string head, string baseBranch, string body)
+    {
+        return await _Client.PullRequest.Create(owner, repo, new NewPullRequest(title, head, baseBranch) { Body = body });
+    }
+
+    public async Task<Release> CreateReleaseAsync(string owner, string repo, string tag, string name, string body)
+    {
+        return await _Client.Repository.Release.Create(owner, repo, new NewRelease(tag) { Name = name, Body = body });
+    }
+
+    #endregion
+
+    public async Task SyncAsync(string repoUrl, string localPath, string commitMessage)
+    {
+        CloneIfMissing(repoUrl, localPath);
+
+        using var repo = OpenRepository(localPath);
+
+        Pull(repo);
+
+        if (HasChanges(repo))
+        {
+            StageAll(repo);
+            Commit(repo, commitMessage);
+            Push(repo);
+        }
+
+    }
+
+}
