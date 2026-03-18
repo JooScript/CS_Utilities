f6f63bbee7719f69827dbd95438a819215195550
Yousef <160142268+JooScript@users.noreply.github.com>
2026-03-18 21:39:07 +0200

    Create Sequence.cs

---
+++ DS/Sequence.cs
@@ -0,0 +1,26 @@
+﻿namespace Utils.DS;
+
+public static class Sequence
+{
+    public static IEnumerable<int> Range(int start, int stop, int step = 1)
+    {
+        if (step == 0)
+            throw new ArgumentException("step cannot be zero");
+
+        if (step > 0)
+        {
+            for (int i = start; i < stop; i += step)
+                yield return i;
+        }
+        else
+        {
+            for (int i = start; i > stop; i += step)
+                yield return i;
+        }
+    }
+
+    public static IEnumerable<int> Range(int stop, int step = 1)
+    {
+        return Range(0, stop, step);
+    }
+}
