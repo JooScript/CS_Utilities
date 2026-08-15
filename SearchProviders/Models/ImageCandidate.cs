5a80b76712428cd2dcb3a4b2f24f6a236b5f3f3a
Yousef <160142268+JooScript@users.noreply.github.com>
2026-08-15 11:35:07 +0300

    Create ImageCandidate.cs

---
+++ SearchProviders/Models/ImageCandidate.cs
@@ -0,0 +1,12 @@
+﻿namespace Utils.SearchProvider.Models;
+
+public sealed class ImageCandidate
+{
+    public string Title { get; init; } = "";
+    public string SourceUrl { get; init; } = "";
+    public string ImageUrl { get; init; } = "";
+    public string Query { get; init; } = "";
+    public int? Width { get; init; }
+    public int? Height { get; init; }
+    public string Provider { get; init; } = "";
+}
