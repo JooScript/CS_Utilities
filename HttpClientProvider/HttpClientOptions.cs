5bf1362520378c5f3cf35fef8562827e6fb2e5ab
Yousef <160142268+JooScript@users.noreply.github.com>
2026-08-15 09:34:54 +0300

    Update HttpClientOptions.cs

--- HttpClientProvider/HttpClientOptions.cs
+++ HttpClientProvider/HttpClientOptions.cs
@@ -1,21 +1,30 @@
-﻿namespace Utils.HttpClientProvider;
+﻿using System.Net;
+
+namespace Utils.HttpClientProvider;
 
 public class HttpClientOptions
 {
-    public TimeSpan PooledConnectionLifetime { get; } = TimeSpan.FromMinutes(15);
+    public TimeSpan PooledConnectionLifetime { get; init; } =
+        TimeSpan.FromMinutes(15);
 
-    public TimeSpan PooledConnectionIdleTimeout { get; } = TimeSpan.FromMinutes(2);
+    public TimeSpan PooledConnectionIdleTimeout { get; init; } =
+        TimeSpan.FromMinutes(2);
 
-    public int MaxConnectionsPerServer { get; } = 8;
+    public int MaxConnectionsPerServer { get; init; } = 8;
 
-    public TimeSpan Timeout { get; } = TimeSpan.FromSeconds(90);
+    public TimeSpan Timeout { get; init; } =
+        TimeSpan.FromSeconds(90);
 
-    public bool AllowAutoRedirect { get; } = true;
+    public bool AllowAutoRedirect { get; init; } = true;
 
-    public string UserAgent { get; } =
+    public DecompressionMethods AutomaticDecompression { get; init; } =
+        DecompressionMethods.All;
+
+    public string UserAgent { get; init; } =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
         "AppleWebKit/537.36 (KHTML, like Gecko) " +
         "Chrome/126.0.0.0 Safari/537.36";
 
-    public string AcceptLanguage { get; } = "en-US,en;q=0.9,ar;q=0.8";
+    public string AcceptLanguage { get; init; } =
+        "en-US,en;q=0.9,ar;q=0.8";
 }
