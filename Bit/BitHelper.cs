91278da7e27452c3f6a4a7970d281f65d2cd21ad
Yousef <160142268+JooScript@users.noreply.github.com>
2026-09-01 17:20:06 +0300

    Create BitHelper.cs

---
+++ Bit/BitHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace Utils.Bit;
+
+public static class BitHelper
+{
+    /// <summary>
+    /// Combines multiple bit flags into a single integer value.
+    /// </summary>
+    public static int Combine(params int[] values)
+    {
+        return values.Aggregate(
+            0,
+            (current, value) => current | value);
+    }
+
+    /// <summary>
+    /// Determines whether the specified value contains the required bit flags.
+    /// </summary>
+    public static bool Has(int required, int value)
+    {
+        return (value & required) == required;
+    }
+}
